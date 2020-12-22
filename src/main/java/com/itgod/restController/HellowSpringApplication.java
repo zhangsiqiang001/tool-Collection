5d2d15f17f32ffb5d4617df70f76c47c7b3324e2
zhangsq <1218542440@qq.com>
2020-12-22 12:39:31 +0800

    开启proxj

--- src/main/java/com/itgod/restController/HellowSpringApplication.java
+++ src/main/java/com/itgod/restController/HellowSpringApplication.java
@@ -2,8 +2,10 @@ package com.itgod.restController;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
 @SpringBootApplication
+@EnableAspectJAutoProxy
 public class HellowSpringApplication {
     public static void main(String[] args) {
         SpringApplication.run(HellowSpringApplication.class, args);
