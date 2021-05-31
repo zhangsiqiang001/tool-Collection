d0c5ec10c63a833012d2136aa8f6a2e5f7070624
zhangsq <1218542440@qq.com>
2021-05-31 17:53:27 +0800

    编写jd定时任务

---
+++ src/main/java/com/itgod/restController/Task_jd/dojdSend.java
@@ -0,0 +1,41 @@
+package com.itgod.restController.Task_jd;
+
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONObject;
+import org.springframework.http.*;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
+
+import javax.annotation.Resource;
+
+/**
+ * @author zhangsq
+ * @date 2021/5/31 11:07
+ */
+@Component
+public class dojdSend {
+    @Resource
+    private RestTemplate restTemplate;
+
+    @Scheduled(cron = "* 0 12 * * ? ")
+    public void dojd_send() {
+        String content = JSON.toJSONString(null);
+        HttpHeaders headers = new HttpHeaders();
+        String xSignature = null;
+        headers.set("X-Signature", xSignature);
+        headers.setContentType(MediaType.APPLICATION_JSON);
+        headers.set("Accept", "*/*");
+        headers.set("Accept-Encoding", "gzip, deflate, br");
+        headers.set("Connection", "keep-alive");
+        HttpEntity<String> httpEntity = new HttpEntity<>(content, headers);
+        ResponseEntity<String> exchange = restTemplate.exchange(null, HttpMethod.POST, httpEntity, String.class);
+        JSONObject jsonObject = JSONObject.parseObject(exchange.getBody());
+        String code = jsonObject.getString("code");
+        if (!"0".equals(code)) {
+
+        }
+        // restTemplate.exchange();
+        System.out.println("定时好了");
+    }
+}
